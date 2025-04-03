bb15b7b1ae3b5f2846ccea833e6447bc8a09e82f
illenko <kostiaillenko@gmail.com>
2025-04-03 09:56:14 +0300

    Add keycloak

--- code-with-quarkus/src/main/kotlin/org/acme/GreetingResource.kt
+++ code-with-quarkus/src/main/kotlin/org/acme/GreetingResource.kt
@@ -3,11 +3,20 @@ package org.acme
 import jakarta.ws.rs.GET
 import jakarta.ws.rs.Path
 import jakarta.ws.rs.Produces
+import jakarta.ws.rs.core.Context
 import jakarta.ws.rs.core.MediaType
+import jakarta.ws.rs.core.SecurityContext
 
 @Path("/hello")
 class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     fun hello() = "Hello from Quarkus REST"
+
+    @GET
+    @Path("/whoami")
+    @Produces(MediaType.TEXT_PLAIN)
+    fun whoAmI(
+        @Context securityContext: SecurityContext,
+    ): String = securityContext.userPrincipal?.name ?: "anonymous"
 }
